1a519e45c7a9172943f3e81b449607b01a485cf7
michalforman <miska.forman@gmail.com>
2024-01-16 22:19:05 +0100

    first version, smartWeather api is working

---
+++ src/controllers/smartWeather/controller.ts
@@ -0,0 +1,59 @@
+// Library imports
+import "dotenv/config";
+import { Request, Response } from "express";
+
+// Custom imports
+import { WeatherData } from "../../interfaces/smartWeather/interfaces.js";
+import {
+  getUmbrellaNeed,
+  getSunscreenNeed,
+  getOutdoorActivities,
+  getOutfit,
+} from "./functions.js";
+
+export const getData = (req: Request, res: Response) => {
+  const cords = req.params.cords;
+  // console.log(cords);
+  const link = `https://api.tomorrow.io/v4/weather/forecast?location=${cords}&apikey=${process.env.TOMORROW_API_KEY}`;
+  fetch(link)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      let weatherData: WeatherData = {
+        umbrellaNeed: undefined,
+        sunscreenNeed: undefined,
+        outdoorActivities: undefined,
+        outfit: undefined,
+      };
+
+      // Umbrella
+      let umbrellaNeed = getUmbrellaNeed(data.timelines.hourly);
+      weatherData.umbrellaNeed = umbrellaNeed;
+
+      // Sunscreen
+      let sunscreenNeed = getSunscreenNeed(
+        data.timelines.daily[0].values.uvIndexMax,
+      );
+      weatherData.sunscreenNeed = sunscreenNeed;
+
+      // Outfit
+      let outfit = getOutfit(
+        data.timelines.daily[0].values.temperatureApparentAvg,
+      );
+      weatherData.outfit = outfit;
+
+      // Outdoor
+      let outdoorActivities = getOutdoorActivities(data.timelines.hourly);
+      weatherData.outdoorActivities = outdoorActivities;
+
+      res.send(weatherData);
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error("Fetch error:", error);
+    });
+};
---
+++ src/controllers/smartWeather/functions.ts
@@ -0,0 +1,74 @@
+import { HourlyData } from "../../interfaces/smartWeather/interfaces.js";
+
+function getSunscreenNeed(uvIndex: number): number {
+  return 1 / (1 + 2 ** (-uvIndex + 3));
+}
+
+function getUmbrellaNeed(hourlyData: HourlyData[]): number {
+  let firstHourIndex: number | undefined;
+  let highestRainIntensity = 0;
+
+  // Get comparable time
+  const today: Date = new Date();
+  today.setMinutes(0, 0, 0);
+  let todayString: string = today.toISOString();
+  todayString = todayString.replace(/\.\d{3}/, "");
+
+  // Find this hour in hourly data
+  hourlyData.forEach((hour, index) => {
+    if (hour.time === todayString) {
+      firstHourIndex = index;
+    }
+  });
+  if (firstHourIndex === undefined) {
+    throw new Error("Could not find first hour index");
+  }
+  const now = new Date().getUTCHours() + 1;
+  console.log(now);
+  const hoursNeeded = 18 - now;
+  for (
+    let i: number = firstHourIndex;
+    i < hoursNeeded + firstHourIndex + 1;
+    i++
+  ) {
+    if (hourlyData[i].values.rainIntensity > highestRainIntensity) {
+      highestRainIntensity = hourlyData[i].values.rainIntensity;
+    }
+  }
+
+  // console.log("Highest rain intensity:", highestRainIntensity);
+  return 1 / (1 + 2 ** (-highestRainIntensity + 2));
+}
+
+function getOutfit(temperature: number): number {
+  return 1 / (1 + 1.123 ** (-temperature + 8.833));
+}
+
+function getOutdoorActivities(hourlyData: HourlyData[]): number {
+  let firstHourIndex: number | undefined;
+  const today: Date = new Date();
+  today.setMinutes(0, 0, 0);
+  let todayString: string = today.toISOString();
+  todayString = todayString.replace(/\.\d{3}/, "");
+
+  // Find this hour in hourly data
+  hourlyData.forEach((hour, index) => {
+    if (hour.time === todayString) {
+      firstHourIndex = index;
+    }
+  });
+  if (firstHourIndex === undefined) {
+    throw new Error("Could not find first hour index");
+  }
+  // get the average rain intensity for the next 16 hours
+  let rainIntensitiesSum: number = 0;
+  for (let i = firstHourIndex; i < 17 + firstHourIndex; i++) {
+    rainIntensitiesSum += hourlyData[i].values.rainIntensity;
+  }
+  const rainIntensity: number = rainIntensitiesSum / 16;
+
+  // console.log(rainIntensity);
+  return 1 / (1 + 100 ** (rainIntensity - 0.2));
+}
+
+export { getSunscreenNeed, getUmbrellaNeed, getOutfit, getOutdoorActivities };
