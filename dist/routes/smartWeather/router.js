a3f6e1e79c3768a0774e20ef6686d97ec8d6bc73
Michal Forman <miska.forman@gmail.com>
2024-02-06 22:41:45 +0100

    add: connection to database and create testing collection

--- dist/routes/smartWeather/router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getData, getTestingData, } from "../../controllers/smartWeather/controller.js";
-const router = express.Router();
-router.get("/production/:coords", getData);
-router.get("/testing/:coords?", getTestingData);
-export default router;
-//# sourceMappingURL=router.js.map
